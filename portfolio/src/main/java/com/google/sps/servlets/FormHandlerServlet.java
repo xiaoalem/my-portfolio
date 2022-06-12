58220936d0a193f1f8a1c837a59365242300509b
Yifan <sueyifankk@gmail.com>
2022-06-12 23:39:14 +0000

    added more css for ContactMe section

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -1,16 +1,15 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 @WebServlet("/form-handler")
 public class FormHandlerServlet extends HttpServlet {
-    ArrayList<String> texts = new ArrayList<>();
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -24,15 +23,5 @@ public class FormHandlerServlet extends HttpServlet {
     // Write the value to the response so the user can see it.
     response.getWriter().println("You submitted: " + textValue);
     response.sendRedirect("https://ysun-sps-summer22.appspot.com/");
-  }
-
-  public ArrayList<String> doGet() {
-
-    // Add words to texts
-    texts.add("Moi");
-    texts.add("Hey Hey");
-    texts.add("Moi Moi");
-
-    return texts;
   }
 }
